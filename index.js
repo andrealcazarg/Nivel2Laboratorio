c9bc2cc0866bf61eef91be590a579f654124746e
andreaag929@gmail.com <Ann20covid>
2022-11-20 21:56:20 +0100

    3rd commit

--- index.js
+++ index.js
@@ -1,37 +1,114 @@
-
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("formulario").addEventListener('submit', validarFormulario);
     });
 
-    function validarFormulario(evento){
-        evento.preventDefault();
-        //input id value
-        var nombre = document.getElementById('nombre').value;
-        //label
-        var respuesta =document.getElementById('label_respuesta');
-        //input id
-        var input = document.getElementById('nombre');
 
-        if(nombre.length == 0) {
-            //error vacio
-            input.style.backgroundImage = "url('images/error-icon.svg')";
-            input.style.backgroundRepeat = 'no-repeat';
-            input.style.backgroundPosition = 'right';
-            respuesta.style.display = 'block';
-            input.style.border = '3px solid red';
-            return;
-        }else{
-            input.style.backgroundImage = "url('images/success-icon.svg')";
-            input.style.backgroundRepeat = 'no-repeat';
-            input.style.backgroundPosition = 'right';
-            input
-            respuesta.style.display = 'none';
-            input.style.border = '3px solid green';
-        }
-        console.log(nombre);
+    function validarFormulario(evento) {
+        evento.preventDefault();
+
+        //Declaracion variables id input
+        var nombre = document.getElementById('nombre');
+        var email = document.getElementById('email');
+        var clave = document.getElementById('clave');
+        var confir_clave = document.getElementById('confir_clave');
+
+        // for(var i = 0; i< respuesta.length; i++){
+        //     if (nombre.length == 0 || email.length == 0){
+        //         respuesta[i].style.display = 'block';
+        //     }
+        // }
+        validarCamposVacios();
+
         // var clave = document.getElementById('clave').value;
-        // if (clave.length < 6) {
+        // if (clave.length < 8) {
         //     alert('La clave no es válida');
         //     return;
         // }
+    }
+
+/**
+ * funcion utilizada para validar si el email es correcto con una expresion regular.
+ * Se tienen en cuenta:
+ * 1: Que empieze por el identificador o nombre del usuario ^\w+([.-_+]?\w+)*
+ * 2: Esta seguido por el @
+ * 3: Nombre dominio del correo (.com, .es) \w+([.-]?\w+)*(\.\w{2,10})+$
+ * Por ejemplo:  andrea@gmail.com
+ */
+    function validarEmail() {
+    var respuesta_email = document.getElementById('label_respuesta_email');
+    var email = document.getElementById('email');
+    var email_valido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    if (email_valido.test(email)) {
+        //error vacio
+        email.style.backgroundImage = "url('images/error-icon.svg')";
+        email.style.backgroundRepeat = 'no-repeat';
+        email.style.backgroundPosition = 'right';
+        email.style.border = '3px solid red';
+       respuesta_email.style.display = 'block';
+        return;
+    } else {
+        email.style.backgroundImage = "url('images/success-icon.svg')";
+        email.style.backgroundRepeat = 'no-repeat';
+        email.style.backgroundPosition = 'right';
+        email.style.border = '3px solid green';
+        respuesta_email.style.display = 'none';
+    }
+        // if (email_valido.test(email)) {
+        //     alert('Email es valido.');
+        //     return true;
+        // } else {
+        //     alert('Email es invalido.');
+        //     return false;
+        // }
+
+}
+
+    function validarCamposVacios() {
+        //Declaracion variables id label
+        var respuesta_nombre = document.getElementById('label_respuesta_nombre');
+        var respuesta_clave = document.getElementById('label_respuesta_clave');
+        var respuesta_clave_longitud = document.getElementById('label_respuesta_clave_longitud');
+        var respuesta_confir_clave = document.getElementById('label_respuesta_confir_clave');
+        var respuesta_clave_no_coincide = document.getElementById('label_respuesta_clave_no_coincide');
+
+       /* if (nombre.value.length == 0) {
+            //error vacio
+            nombre.style.backgroundImage = "url('images/error-icon.svg')";
+            nombre.style.backgroundRepeat = 'no-repeat';
+            nombre.style.backgroundPosition = 'right';
+            nombre.style.border = '3px solid red';
+            respuesta_nombre.style.display = 'block';
+            return;
+        } else {
+            nombre.style.backgroundImage = "url('images/success-icon.svg')";
+            nombre.style.backgroundRepeat = 'no-repeat';
+            nombre.style.backgroundPosition = 'right';
+            nombre.style.border = '3px solid green';
+            respuesta_nombre.style.display = 'none';
+        }*/
+
+        validarEmail();
+      /*  if (clave.value.length == 0) {
+            //error vacio
+            clave.style.backgroundImage = "url('images/error-icon.svg')";
+            clave.style.backgroundRepeat = 'no-repeat';
+            clave.style.backgroundPosition = 'right';
+            clave.style.border = '3px solid red';
+            respuesta_clave.style.display = 'block';
+            return;
+        } else if (clave.value.length < 8){
+            clave.style.backgroundImage = "url('images/error-icon.svg')";
+            clave.style.backgroundRepeat = 'no-repeat';
+            clave.style.backgroundPosition = 'right';
+            clave.style.border = '3px solid red';
+            respuesta_clave_longitud.style.display = 'block';
+        }else{
+            clave.style.backgroundImage = "url('images/success-icon.svg')";
+            clave.style.backgroundRepeat = 'no-repeat';
+            clave.style.backgroundPosition = 'right';
+            clave.style.border = '3px solid green';
+            respuesta_clave.style.display = 'none';
+            respuesta_clave_longitud.style.display = 'none';
+        }*/
+
     }
