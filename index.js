fa25250d2ee4a56cc69d59abe11d906b17ac935d
andreaag929@gmail.com <Ann20covid>
2023-01-28 21:54:33 +0100

    6rd commit

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
 var inputs = document.getElementsByTagName('input');
 
 
-
 function validarFormulario(evento) {
     evento.preventDefault();
     validarCamposVacios();
@@ -47,13 +46,12 @@ function validar(campo) {
     var confir_clave = document.getElementById('confir_clave');
     var respuesta_clave_longitud = document.getElementById('label_respuesta_clave_longitud');
     var respuesta_clave_no_coincide = document.getElementById('label_respuesta_clave_no_coinciden');
-    var respuesta_email_invalido= document.getElementById('label_respuesta_email_invalido');
+    var respuesta_email_invalido = document.getElementById('label_respuesta_email_invalido');
 
     if (campo.value) {
-        //si existe nombnre OK. si existe password tengo que validar:
+        //si existe nombre OK. si existe password tengo que validar:
         //1:que tenga al menos 8 caracteres y que password y confirm_password sean iguales.
-
-
+        //2:que el email sea valido
 
         if (!(clave.value.length >= 8 && confir_clave.value.length >= 8) && (clave.value == confir_clave.value) && (clave.value.length != 0 || confir_clave.value.length != 0)) {
             //estan VACIOS, NO ES MAYOR O IGUAL A 8 Y SON IGUALES
@@ -63,12 +61,12 @@ function validar(campo) {
             //    document.getElementById('label_respuesta_' + campo.id).style.display = 'block';
             clave.style.backgroundImage = "url('images/error-icon.svg')";
             clave.style.backgroundRepeat = 'no-repeat';
-            clave.style.backgroundPosition = 'right';
+            clave.style.backgroundPosition = 'right 1% bottom 45%';
             clave.style.border = '3px solid red';
-            confir_clave.style.backgroundImage = "url('images/error-icon.svg')";
+            confir_clave.style.backgroundImage = "url('images/success-icon.svg')";
             confir_clave.style.backgroundRepeat = 'no-repeat';
-            confir_clave.style.backgroundPosition = 'right';
-            confir_clave.style.border = '3px solid red';
+            confir_clave.style.backgroundPosition = 'right 1% bottom 45%';
+            confir_clave.style.border = '3px solid green';
 
         } else if (!(clave.value.length >= 8 && confir_clave.value.length >= 8) && !(clave.value == confir_clave.value)) {
             //NO ES MAYOR IGUAL A 8, NO SON IGUALES
@@ -78,11 +76,11 @@ function validar(campo) {
             //document.getElementById('label_respuesta_' + campo.id).style.display = 'block';
             clave.style.backgroundImage = "url('images/error-icon.svg')";
             clave.style.backgroundRepeat = 'no-repeat';
-            clave.style.backgroundPosition = 'right';
+            clave.style.backgroundPosition = 'right 1% bottom 45%';
             clave.style.border = '3px solid red';
             confir_clave.style.backgroundImage = "url('images/error-icon.svg')";
             confir_clave.style.backgroundRepeat = 'no-repeat';
-            confir_clave.style.backgroundPosition = 'right';
+            confir_clave.style.backgroundPosition = 'right 1% bottom 45%';
             confir_clave.style.border = '3px solid red';
 
         } else if ((clave.value.length >= 8 && confir_clave.value.length >= 8) && !(clave.value == confir_clave.value)) {
@@ -92,11 +90,11 @@ function validar(campo) {
             // document.getElementById('label_respuesta_' + campo.id).style.display = 'block';
             clave.style.backgroundImage = "url('images/error-icon.svg')";
             clave.style.backgroundRepeat = 'no-repeat';
-            clave.style.backgroundPosition = 'right';
+            clave.style.backgroundPosition = 'right 1% bottom 45%';
             clave.style.border = '3px solid red';
             confir_clave.style.backgroundImage = "url('images/error-icon.svg')";
             confir_clave.style.backgroundRepeat = 'no-repeat';
-            confir_clave.style.backgroundPosition = 'right';
+            confir_clave.style.backgroundPosition = 'right 1% bottom 45%';
             confir_clave.style.border = '3px solid red';
 
         } else if ((clave.value.length >= 8 && confir_clave.value.length >= 8) && (clave.value == confir_clave.value)) {
@@ -107,28 +105,28 @@ function validar(campo) {
             document.getElementById('label_respuesta_' + campo.id).style.display = 'none';
             campo.style.backgroundImage = "url('images/success-icon.svg')";
             campo.style.backgroundRepeat = 'no-repeat';
-            campo.style.backgroundPosition = 'right';
+            campo.style.backgroundPosition = 'right 1% bottom 45%';
             campo.style.border = '3px solid green';
 
         } else {
             document.getElementById('label_respuesta_' + campo.id).style.display = 'none';
             campo.style.backgroundImage = "url('images/success-icon.svg')";
             campo.style.backgroundRepeat = 'no-repeat';
-            campo.style.backgroundPosition = 'right';
+            campo.style.backgroundPosition = 'right 1% bottom 45%';
             campo.style.border = '3px solid green';
         }
-        if(validarEmail()){
+        if (validarEmail()) {
             respuesta_email_invalido.style.display = 'block';
             email.style.backgroundImage = "url('images/error-icon.svg')";
             email.style.backgroundRepeat = 'no-repeat';
-            email.style.backgroundPosition = 'right';
+            email.style.backgroundPosition = 'right 1% bottom 45%';
             email.style.border = '3px solid red';
             document.getElementById('label_respuesta_' + campo.id).style.display = 'none';
-        }else{
+        } else {
             respuesta_email_invalido.style.display = 'none';
             email.style.backgroundImage = "url('images/success-icon.svg')";
             email.style.backgroundRepeat = 'no-repeat';
-            email.style.backgroundPosition = 'right';
+            email.style.backgroundPosition = 'right 1% bottom 45%';
             email.style.border = '3px solid green';
             document.getElementById('label_respuesta_' + campo.id).style.display = 'none';
         }
@@ -139,7 +137,7 @@ function validar(campo) {
         document.getElementById('label_respuesta_' + campo.id).style.display = 'block';
         campo.style.backgroundImage = "url('images/error-icon.svg')";
         campo.style.backgroundRepeat = 'no-repeat';
-        campo.style.backgroundPosition = 'right';
+        campo.style.backgroundPosition = 'right 1% bottom 45%';
         campo.style.border = '3px solid red';
     }
 
